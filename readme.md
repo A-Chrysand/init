e933d3733fcfde9aa34d01a8193bd221f2a603df
A_Chrysand <achrysand@gmail.com>
2022-01-15 10:43:25 +0800

    init

---
+++ readme.md
@@ -0,0 +1,89 @@
+﻿# init porgram
+
+### temp
+|fileName|function|target|
+|:----:|:----:|:----:|
+| ce.py  | set JAVAHOME | full remake|
+| ide.exe | quick run ide| add config file support|
+| ipa.bat | ipconfig /all| full remake|
+| vi.ps1| quick nodepad.exe edit file| full remake|
+|vmrun.py| start vm in cmd(used in SSH)| full remake|
+
+
+
+
+## description
+
+This is just only some/something quick launch command SET program.  
+<em>Actually this program's functions are THE SAME AS the bat/ps1 shell script or edit Windows Register table</em>   
+Because ```.bat``` is so awful and ```.ps1``` is not able to run directly in Win+R (you need to
+run ```powershell xxx.ps1``` instead of ```xxx.ps1```)    
+### file description
+Program: ```%dir%/init.exe```, used to execute some commandline command.  
+Conifg: ```%dir%/config/init.ini```, used to storage some custom
+
+## usage
+
+1. Set your custom cmd in ```config/init.ini```.  
+    In ``init.ini``, every things are like common config file, one section must contain 3 keyname: ```AlterName```,```ExecuteCmd```,and at least 1 custom command.  
+    Tips:
+    1. ```AlterName``` and ```ExecuteCmd```  themselves are case sensitive. Please pay attention to the case while editing the file.  
+    2. Path are supposed to add " " like ```"D:\\software\\test.exe"```(√) instead of ```D:\\software\\test.exe```(×) 
+   
+
+
+
+```ini
+[test]
+AlterName = t,test,test0,exampleTest,    #[required] altername is use to execute as parameter
+ExecuteCmd = game111,cmdtwo...           #[required] cmd is the all [commandline commands] will be executed and linked to the name you set below
+game111 = "G:\\steam\\steam.exe"         #[required] ### You can use cmd to start some software
+cmdtwo = "code D:\\"                     #[optional] ### Of course add some parameters is enabled
+```
+
+2. run init.exe in commandline or run(Win+R)
+
+```shell   
+init %para1% %para2% %para3%...
+```
+
+these parameter is what you have set in ```init.ini```. Example:
+
+```
+init test
+>>>steam.exe VisualStudioCode pycharm started
+```
+
+## Program mermaid
+```flow
+      (start)
+         |
+         |
+         ↓
+  [init project]=>[| process cmd parameter && read .ini file]
+         |
+         ↓
+<foreach: secitons>----(section==null)---->(Program Exit)
+         | 
+  (section!=null)
+         ↓   
+[process section 'AlterName', get AlterNameList<string>]
+         |
+         |
+         ↓
+ <AlterName==null?>----YES---->(Program Exit)
+         |
+         |NO
+         ↓
+[process AlterNameList<string> get targetExecuteCMD]
+         |
+         |
+         ↓
+   [execute cmd]
+         |
+         |
+         ↓
+      （END)
+
+
+```
---
+++ src/init_main.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IniParser;
+using IniParser.Model;
+
+namespace init {
+    class main {
+        private static void Main(string[] args)
+        {
+            //init program
+            if (args.Length == 0) {
+                Console.WriteLine("Error>>>缺少参数");
+                return;
+            }
+            else {
+                foreach (var args_item in args) {
+                    Execute(args_item);
+                }
+            }
+        }
+
+        public static void Execute(string inParameter)
+        {
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile("D:\\System Software\\CMDitem\\config\\init.ini");
+            //set search result
+            bool bool_MatchItemSuccessfully = false;
+            //Do the game:
+            foreach (var iSectionName in data.Sections) {
+                //get AlterName string
+                string tmp_str_sectionAlterName = data[iSectionName.SectionName]["AlterName"];
+                if (tmp_str_sectionAlterName == null) {
+                    Console.WriteLine("Error>>>name 'AlterName' is empty. Please check file init.ini. Program exit");
+                    Environment.Exit(-1);
+                }
+                //split [string]AlterName into List<string>AlterName
+                List<string> tmp_list_sectionAlterName = CutString(tmp_str_sectionAlterName, ',');
+                foreach (string part in tmp_list_sectionAlterName) {
+                    if (part.Equals(inParameter)) {
+                        //get ExecuteCmd string
+                        string str_sectionCMD = data[iSectionName.SectionName]["ExecuteCmd"];
+                        if (str_sectionCMD == null) {
+                            Console.WriteLine("Error>>>name 'ExecuteCmd' is empty. Please check file init.ini");
+                        }
+                        //split [string]ExecuteCmd into List<string>ExecuteCmd
+                        List<string> list_sectionCMD = CutString(str_sectionCMD, ',');
+                        foreach (string Cmd_name_item in list_sectionCMD) {
+                            // get value(the things you are going to execute in shell) by name indexing
+                            string tmp_cmd_value = data[iSectionName.SectionName][Cmd_name_item];
+                            // if the value is not null and then execute the command
+                            if (tmp_cmd_value != null) {
+                                if (tmp_cmd_value.Equals("") == false) {
+                                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                                    System.Diagnostics.ProcessStartInfo startInfo =
+                                        new System.Diagnostics.ProcessStartInfo();
+                                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                                    startInfo.FileName = "cmd.exe";
+                                    startInfo.Arguments = "/C " + tmp_cmd_value;
+                                    process.StartInfo = startInfo;
+                                    process.Start();
+                                }
+                            }
+                            else {
+                                Console.WriteLine("Error>>>value <" + Cmd_name_item +
+                                                  "> Do not associate to name. Please check file init.ini");
+                            }
+                        }
+                        bool_MatchItemSuccessfully = true;
+                        break;
+                    }
+                }
+                if (bool_MatchItemSuccessfully) {
+                    break;
+                }
+            }
+            if (!bool_MatchItemSuccessfully) {
+                Console.WriteLine("ERROR>>>No section found, search cmd keyword<" + inParameter +
+                                  "> failed, please check your init.ini file or the parameter entered");
+            }
+        }
+
+
+        public static List<string> CutString(String SourceString, char SplitTarget)
+        {
+            List<string> tmp_list_SourceString = new List<string>();
+            try {
+                string[] temp = SourceString.Split(SplitTarget);
+                tmp_list_SourceString = temp.ToList();
+            }
+            catch (NullReferenceException) {
+                tmp_list_SourceString.Add(SourceString[0].ToString());
+            }
+            return tmp_list_SourceString;
+        }
+    }
+}
